4dcaaa6b9f576c095e770ba5e257ed24ececb2cb
Olaf Klischat <o.klischat@syseleven.de>
2019-11-14 22:04:28 +0100

    mark pod as ready functionality

--- go.mod
+++ go.mod
@@ -9,7 +9,11 @@ require (
 	golang.org/x/net v0.0.0-20191112182307-2180aed22343 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	k8s.io/apimachinery v0.0.0-20191114135336-bcf004c497a4 // indirect
+	k8s.io/api v0.0.0-20191016110246-af539daaa43a
+	k8s.io/apimachinery v0.0.0-20191114135336-bcf004c497a4
 	k8s.io/client-go v0.0.0-20191016110837-54936ba21026
+	k8s.io/klog v1.0.0
 	k8s.io/utils v0.0.0-20191114184206-e782cd3c129f // indirect
 )
+
+replace k8s.io/kubernetes => /Users/oklischat/go/src/k8s.io/kubernetes
--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
     "flag"
-    "fmt"
-    "github.com/golang/glog"
+    v1 "k8s.io/api/core/v1"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/tools/clientcmd"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/klog"
     "os"
 )
 
-var (
-    kubeconfigPath string
-)
-
-
 func main() {
+    var (
+        kubeconfigPath string
+        namespace string
+    )
+
     flag.StringVar(&kubeconfigPath, "kubeconfigPath", "", "Path to a kubeconfigPath.")
+    flag.StringVar(&namespace, "namespace", "", "Path to a kubeconfigPath.")
 
     flag.Parse()
 
@@ -23,15 +25,50 @@ func main() {
         kubeconfigPath = os.Getenv("KUBECONFIG")
     }
 
+    if namespace == "" {
+        namespace = "default"
+    }
+
+    podName := flag.Arg(0)
+    if podName == "" {
+        klog.Fatalf("podUpdate name not specified")
+    }
+
     kubeconfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
     if err != nil {
-        glog.Fatalf("error building kubeconfigPath: %v", err)
+        klog.Fatalf("error getting kubeconfig: %v", err)
     }
 
     kubeClient, err := kubernetes.NewForConfig(kubeconfig)
     if err != nil {
-        glog.Fatalf("error building kubernetes clientset for kubeClient: %v", err)
+        klog.Fatalf("error building kubernetes client: %v", err)
     }
 
-    fmt.Printf("got kubeClient: %v", kubeClient)
+    pod, err := kubeClient.CoreV1().Pods(namespace).Get(podName, metav1.GetOptions{})
+    if err != nil {
+        klog.Fatalf("error getting pod %s/%s: %v", namespace, podName, err)
+    }
+
+    // copied (incl. dependencies) and adapted from https://github.com/kubernetes/kubernetes/blob/v1.15.5/pkg/controller/util/node/controller_utils.go#L122
+    podUpdate := pod.DeepCopy()
+    for _, cond := range podUpdate.Status.Conditions {
+        if cond.Type == v1.PodReady {
+            cond.Status = v1.ConditionTrue
+            if !UpdatePodCondition(&podUpdate.Status, &cond) {
+                break
+            }
+            klog.V(2).Infof("Updating ready status of pod %v to true", podUpdate.Name)
+            _, err := kubeClient.CoreV1().Pods(podUpdate.Namespace).UpdateStatus(podUpdate)
+            if err != nil {
+                klog.Fatalf("Failed to update status: %v", err)
+            }
+            break
+        }
+    }
+
+    if _, err := kubeClient.CoreV1().Pods(podUpdate.Namespace).UpdateStatus(podUpdate); err != nil {
+        klog.Fatalf("Failed to update status: %v", err)
+    }
+
+    klog.Infof("Successfully updated pod %s/%s", namespace, podName)
 }
