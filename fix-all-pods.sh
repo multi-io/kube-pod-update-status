9e19c9e555c7e59dd07328d6c83db2db0087cd03
Olaf Klischat <o.klischat@syseleven.de>
2020-02-28 14:01:26 +0100

    fix-all-pods.sh: filter out pods that do not have .status.conditions (may happen e.g. for evicted pods)

--- fix-all-pods.sh
+++ fix-all-pods.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl get pod --all-namespaces -o json | jq -r '.items[] | {"namespace": .metadata.namespace, "name": .metadata.name, "ContainersReady": .status.conditions[] | select(.type|contains("ContainersReady")) | .status, "Ready": .status.conditions[] | select(.type|contains("Ready")) | .status } | select(.Ready=="False" and .ContainersReady=="True") | .namespace + " " + .name' | while read namespace name; do
+kubectl get pod --all-namespaces -o json | jq -r '.items[] | select(.status | has("conditions")) | {"namespace": .metadata.namespace, "name": .metadata.name, "ContainersReady": .status.conditions[] | select(.type|contains("ContainersReady")) | .status, "Ready": .status.conditions[] | select(.type|contains("Ready")) | .status } | select(.Ready=="False" and .ContainersReady=="True") | .namespace + " " + .name' | while read namespace name; do
   echo "processing pod: ${namespace}/${name}" >&2
   if [[ -z "$DRYRUN" ]]; then
     if ! ./kube-pod-update-status --namespace "$namespace" "$name"; then
